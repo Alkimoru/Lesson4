70fa093f2a46881b35248e5bb076be8e1e758053
Alkimoru <plehanovdaniil91@gmail.com>
2024-02-16 13:41:32 +0300

    Загрузил задания

---
+++ Task2/Program.cs
@@ -0,0 +1,40 @@
+﻿void Generation(int[] collection)
+{
+    int index_generation = 0;
+    while(index_generation < collection.Length)
+    {
+        collection[index_generation] = new Random().Next(100, 1000);
+        index_generation++;
+    }
+}
+void PrintArray(int[] print)
+{
+    int position = 0;
+    Console.Write("{");
+    while(position < print.Length)
+    {
+        
+        Console.Write(print[position] );
+        Console.Write(", ");
+        
+        position++;
+    }
+    Console.Write("}");
+    Console.WriteLine(" ");
+}
+int[] array = new int[10];
+Generation(array);
+PrintArray(array);
+int index = 0;
+int result = 0;
+while (index < array.Length)
+{
+    if (array[index] % 2 == 0)
+    {
+        result++;
+    }
+
+    index++;
+}
+Console.Write("Колличество чётных элементов в массиве равно: ");
+Console.Write(result);
---
+++ Task3/Program.cs
@@ -0,0 +1,36 @@
+﻿void Generation(int[] collection)
+{
+    int index_generation = 0;
+    while(index_generation < collection.Length)
+    {
+        collection[index_generation] = new Random().Next(100, 1000);
+        index_generation++;
+    }
+}
+void PrintArray(int[] print)
+{
+    int position = 0;
+    Console.Write("{");
+    while(position < print.Length)
+    {
+        
+        Console.Write(print[position] );
+        Console.Write(", ");
+        
+        position++;
+    }
+    Console.Write("}");
+    Console.WriteLine(" ");
+}
+int[] array = new int[10];
+int temp;
+Generation(array);
+PrintArray(array);
+for (int i = 0; i < array.Length / 2; i++)
+{
+    temp = array[i];
+    array[i] = array[array.Length - 1 - i]; 
+    array[array.Length - 1 - i] = temp; 
+}
+Console.Write("\nПеревернутый массив: ");
+PrintArray(array);
